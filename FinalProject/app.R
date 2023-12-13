dfb9d00c8bb8a75d281eeafee9e62301edf04ac3
Christian Landaverde <cland@scc-wi2.scc.bu.edu>
2023-12-12 23:18:39 -0500

    project work

--- FinalProject/app.R
+++ FinalProject/app.R
@@ -15,6 +15,7 @@ library(tools)
 library(colourpicker) # you might need to install this
 library(DT)
 library(matrixStats)
+library(gplots)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -53,8 +54,8 @@ ui <- fluidPage(
                      
                      tabPanel("Summary", DTOutput("Csummary")),
                      tabPanel("Diagnostic Plots", plotOutput("CDiagplot1"), plotOutput("CDiagplot2")),
-                     tabPanel("Heatmap"),
-                     tabPanel("PCA")
+                     tabPanel("Heatmap", plotOutput("Cheatmap")),
+                     tabPanel("PCA", plotOutput("CPCA"))
                     )
                   )
                 )
@@ -171,6 +172,7 @@ server <- function(input, output) {
   
   # filtering variance and non zero genes
   filter_zero_var_genes <- function(verse_counts, button_variance, button_nonzero, num_samples, num_genes) {
+    #browser()
     # Compute variance for each gene
     variance <- apply(verse_counts[, -1], 1, var)
     
@@ -182,6 +184,12 @@ server <- function(input, output) {
     
     # Apply the filters to get the final counts matrix
     filtered_counts <- verse_counts[filtered_genes, ]
+    
+    return(filtered_counts)
+  }
+    
+  create_summary_df <- function(filtered_counts, num_samples, num_genes) {
+  
     percentage_passing <- round((nrow(filtered_counts) / num_genes) * 100, 2)
     not_passing <- num_genes - nrow(filtered_counts)
     percentage_not_passing <- round((not_passing / num_genes) * 100, 2)
@@ -196,51 +204,113 @@ server <- function(input, output) {
   }
   
   # plotting median vs variance
-  plot_variance_vs_median <- function(data, button_variance, scale_y_axis=TRUE, title="") {
-    #browser()
-    matrix_data <- matrix(data[-1,-1])
-    median_val <- rowMedians(matrix_data)
-    calc_variance <- apply(matrix_data, 1, var)
-    filter_variance <- calc_variance > button_variance
+  plot_variance_vs_median <- function(data, scale_y_axis=TRUE, scale_x_axis=TRUE, title="Plot of Median vs Variance") {
+    median_val <- apply(data[,-1], 1, median, na.rm = TRUE)
+    vector_variance <- apply(data[,-1], 1, var, na.rm = TRUE)
+    df_data <- data.frame(Median_count = median_val, Variance = vector_variance)
     
-    df_data <- data.frame(Median_count = median_val, Variance = filter_variance)
+    df_data$PassThreshold <- df_data$Variance > input$countsVariance
     
-    df_data$Rank <- rank(median_val)
+    custom_colors <- c("FALSE" = "red", "TRUE" = "black")
     
-    plot_val <- ggplot(df_data, aes(x = Rank, y = Variance)) +
+    plot_val <- ggplot(df_data, aes(x = Median_count, y = Variance, color = PassThreshold)) +
       geom_point() +
-      labs(x='Rank(Mean)', y='Variance') +
-      geom_abline() +
-      geom_smooth()
+      labs(x='Median', y='Variance') +
+      scale_color_manual(values = custom_colors) +
     ggtitle(title)
-    if (scale_y_axis) {
-      plot_val <- plot_val + scale_y_log10()
+    if (scale_y_axis & scale_x_axis) {
+      plot_val <- plot_val + scale_y_log10() 
+      plot_val <- plot_val + scale_x_log10()
     }
     
     return(plot_val)
     
   }
   
+  plot_zeros_vs_median <- function(data, title="Plot of Median vs Number of Non-Zero genes") {
+    #browser()
+    median_val <- apply(data[,-1], 1, median, na.rm = TRUE)
+    # Count the number of zeros in each row
+    num_zeros <- rowSums(data[,-1] == 0, na.rm = TRUE)
+    
+    log_med <- log2(median_val + 1)
+    log_zero <- log2(num_zeros + 1)
+    
+    # Create a dataframe for ggplot
+    plot_data <- data.frame(Median_count = log_med, Num_zeros = log_zero)
+    plot_data$PassThreshold <- plot_data$Num_zeros < input$countsNonzero
+    
+    custom_colors <- c("FALSE" = "red", "TRUE" = "black")
+    
+    plot_zero_val <- ggplot(plot_data, aes(x = Median_count, y = Num_zeros, color = PassThreshold)) +
+      geom_point() +
+      labs(x='Median', y='Number of samples with zero count') +
+      scale_color_manual(values = custom_colors)
+      ggtitle(title)
+    return(plot_zero_val)
+  }
+  
+  
+  counts_heatmap <- function(data, filtered_counts, button_variance, button_nonzero) {
+    subset_counts <- filtered_counts[,-1]
+    counts_matrix <- as.matrix(subset_counts)
+    counts_matrix_log <- log2(counts_matrix + 1)
+    heatmap.2(counts_matrix_log)
+    
+  }
+  
+  plot_pca <- function(data, title="") {
+    pca_result <- prcomp(t(data), center = TRUE)
+    
+    # in the pac_result i will access the component that the user specifies
+    pca_data <- data.frame(PC1 = pca_result$x[, 1], PC2 = pca_result$x[, 2])
+    
+    pca_plot <- ggplot(data = pca_data, aes(x = PC1, y = PC2, color=timepoint)) +
+      geom_point() +
+      labs(title = title, x = "PC1: 75% Variance", y = "PC2: 15% Variance")
+    
+    return(pca_plot)
+  }
+  
+    
+    
+    
+  # Summary table
   output$Csummary <- renderDT({
     input$countsSubmit
     c_file <<- counts_load_data()
-    num_samples <-  ncol(c_file)
-    num_genes <- nrow(c_file)
-    isolate({filter_zero_var_genes(c_file, input$countsVariance, input$countsNonzero, num_samples, num_genes)})
+    num_samples <<-  ncol(c_file)
+    num_genes <<- nrow(c_file)
+    isolate({filtered_counts <- filter_zero_var_genes(c_file, input$countsVariance, input$countsNonzero)})
+    isolate({create_summary_df(filtered_counts, num_samples, num_genes)})
   })
   
-  
+  # Diagnostic plot
   output$CDiagplot1 <- renderPlot({
     input$countsSubmit
-    isolate({plot_variance_vs_median(c_file, input$countsVariance)})
+    #isolate({filtered_counts <- filter_zero_var_genes(c_file, input$countsVariance, input$countsNonzero)})
+    isolate({plot_variance_vs_median(c_file)})
+    
+  })
+  # Diagnostic plot
+  output$CDiagplot2 <- renderPlot({
+    input$countsSubmit
+    #isolate({filtered_counts <- filter_zero_var_genes(c_file, input$countsVariance, input$countsNonzero)})
+    isolate({plot_zeros_vs_median(c_file)})
     
   })
   
-  output$CDiagplot2 <- renderPlot({
-    
-    
-    
+  # Heatmap
+  output$Cheatmap <- renderPlot({
+    input$countsSubmit
+    isolate({filtered_counts <- filter_zero_var_genes(c_file, input$countsVariance, input$countsNonzero)})
+    isolate({counts_heatmap(c_file, filtered_counts, input$countsVariance, input$countsNonzero)})
+  
   })
+  
+  #output$CPCA <- renderPlot({
+    
+  #})
   
   
   
